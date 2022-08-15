31af97a3e33cd19a6cbc8d5c3c5d58eef5ed9b2a
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2022-08-15 14:50:06 +0300

    add confetti

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import './Result.css'
+import React from "react";
+import "./Result.css";
+import Confetti from "react-confetti";
+import useWindowSize from "react-use/lib/useWindowSize";
 
 const Result: React.FC = () => {
+  const { width, height } = useWindowSize();
 
-   return (
-  <section className='Result container'>
-    <h3>You win!</h3>
-    <img src='./images/frog.png' alt=''/>
-    <button>Play again</button>
-  </section>
-
-)}
+  return (
+    <section className="Result container">
+      <Confetti width={width} height={height} />
+      <h3>You win!</h3>
+      <img src="./images/frog.png" alt="" />
+      <button>Play again</button>
+    </section>
+  );
+};
 
 export default Result;
